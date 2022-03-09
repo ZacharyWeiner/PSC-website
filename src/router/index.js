df3a6c0c0cf4b61f3c5b9c2f28cad33d17ba3368
unknown <stevenkless@gmail.com>
2022-03-09 18:57:41 -0500

    added new firebase functions

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import PreorderCheck from '../views/PreorderCheck.vue'
+import Check from '../views/Check.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Preorder Check',
     component: PreorderCheck
   }, 
+  {
+    path: '/check',
+    name: 'Check',
+    component: Check
+  }, 
 ]
 
 const router = createRouter({
@@ -21,3 +27,5 @@ const router = createRouter({
 })
 
 export default router
+  
+  
---
+++ src/views/Check.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="flex w-full justify-items-center">
+        <div class='w-auto mx-auto md:mx-4'>
+            <div class="text-white">
+                {{allOrders}}
+            </div>
+        <button
+            @click="markMiinted(1)"
+            type="button"
+            class="inline-flex items-center px-1 md:px-3.5 py-2 my-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+            Mark Minted
+        </button>
+        <button
+            @click="sendOrder('234565432', 'otherSteve', 'sgfdgsdgiufdogsufigod', 101, false)"
+            type="button"
+            class="inline-flex items-center px-1 md:px-3.5 py-2 my-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+            Send Order
+        </button>
+
+        <div class="text-white">
+                {{orders}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { useOrders }from '../services/firebase'
+export default {
+    setup() {
+    
+        const {allOrders , sendOrder, findOrders, markMiinted} = useOrders() 
+
+        const orders = findOrders('otherSteve')
+
+        return { allOrders, sendOrder, orders, markMiinted }
+
+    },
+    methods: {
+
+    }
+
+
+
+}
+</script>
