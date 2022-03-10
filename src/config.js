e5c188b9e0432e0c8faa3e177ec2db3f29a17a82
Zack Wins <el_hefe@mbp.attlocal.net>
2022-03-10 12:43:50 -0500

    updated the git config to include config.js

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+    apiKey: "AIzaSyDrWjKrSlO_Ze1iYWqUWda641AHy9TKo-8",
+    authDomain: "mint-choclate.firebaseapp.com",
+    projectId: "mint-choclate",
+    storageBucket: "mint-choclate.appspot.com",
+    messagingSenderId: "407689808989",
+    appId: "1:407689808989:web:2a3057cce42ef4fff191b4",
+    mintPayPrivKey: "Kz9dbsm8BP1DLJW112W3nUEC6nEHCQiyLX1U6PZLRUspe8B98nEo",
+    mintOwnerPrivKey: "KyB4dK8kWBEhieyW4kqEa2zkNWBeyyg47dDsPAPsbXJ9YbxCiyYC"
+}
+export {
+  config
+}
