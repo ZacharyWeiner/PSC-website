001949dd993554d3137aebf7e50b0a64f25fd214
Zack Wins <el_hefe@mbp.attlocal.net>
2022-03-29 21:21:19 -0400

    merged in changes from updates over the alst week - pages for treasury, my-bag, and orders on relay, new UI elements and page navigation

--- src/config.js
+++ src/config.js
@@ -1,20 +1,10 @@
-//Mint Chocolate
-// const config = {
-//     apiKey: "AIzaSyDrWjKrSlO_Ze1iYWqUWda641AHy9TKo-8",
-//     authDomain: "mint-choclate.firebaseapp.com",
-//     projectId: "chat-af0ae",
-//     storageBucket: "mint-choclate.appspot.com",
-//     messagingSenderId: "407689808989",
-//     appId: "1:407689808989:web:2a3057cce42ef4fff191b4",
-// }
-
 const config = {
-    apiKey: "AIzaSyBFhv9vBqa6rnzxVBXQn2nVcSW3r14q-IY",
-    authDomain: "chat-af0ae.firebaseapp.com",
-    projectId: "chat-af0ae",
-    storageBucket: "chat-af0ae.appspot.com",
-    messagingSenderId: "926708550672",
-    appId: "1:926708550672:web:cfc64e3c9f2eb9ff9fce95",
+    apiKey: "",
+    authDomain: "*.firebaseapp.com",
+    projectId: "",
+    storageBucket: ".appspot.com",
+    messagingSenderId: "",
+    appId: "",
 }
 export {
   config
