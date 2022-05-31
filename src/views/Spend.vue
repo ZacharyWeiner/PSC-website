667b6103b9b2430deaa133a988859b690e58e333
Zack Wins <el_hefe@mbp.attlocal.net>
2022-05-31 07:58:50 -0400

    added spend page

---
+++ src/views/Spend.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+    setup () {
+        const state = reactive({
+            count: 0,
+        })
+    
+        return {
+            ...toRefs(state),
+        }
+    },
+    methods:{
+        async spend(){
+            let r1 = window.relayone.alpha.run.getOwner();
+            console.log(r1)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
