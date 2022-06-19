ef7bfbae24770396e77bc57b7104d490667515e9
Steven K <stevenkless@gmail.com>
2022-06-19 19:49:53 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const config = {
-    apiKey: "AIzaSyBFhv9vBqa6rnzxVBXQn2nVcSW3r14q-IY",
-    authDomain: "chat-af0ae.firebaseapp.com",
-    projectId: "chat-af0ae",
-    storageBucket: "chat-af0ae.appspot.com",
-    messagingSenderId: "926708550672",
-    appId: "1:926708550672:web:cfc64e3c9f2eb9ff9fce95"
+    apiKey: "",
+    authDomain: "*.firebaseapp.com",
+    projectId: "",
+    storageBucket: ".appspot.com",
+    messagingSenderId: "",
+    appId: "",
 }
 export {
   config
