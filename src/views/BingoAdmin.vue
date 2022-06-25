55a913af4a1eb8c9bdafbe90c533549fc924fa4e
Zack Wins <el_hefe@mbp.attlocal.net>
2022-06-25 10:49:54 -0400

    bingo admin usability & display updates

--- src/views/BingoAdmin.vue
+++ src/views/BingoAdmin.vue
@@ -1,16 +1,11 @@
 <template>
-<!-- <button @click="newGame()"
-    class="p-2 m-2 text-white text-xl bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl"
->
-Create New Game
-</button> -->
+ <Menu />
+    <div class="bg-gray-100 w-full text-gray-900">
+        Game: <span v-if="currentGame && currentGame[0]">{{currentGame[0].id}}</span>
+    </div>
     <div class="container m-auto my-20 text-white">
         <div v-for="game in currentGame"
             :key="game.id">
-
-            <!-- {{game}} -->
-            
-            
             <div v-if="game.gameComplete">
                 <button @click="newGame()"
                         class="p-2 m-2 text-white text-xl bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl"
@@ -20,54 +15,66 @@ Create New Game
             </div>
 
             <div v-else>
-                <div class="text-left">
-                    Game ID: {{game.id}}
-                    <br><br>
-                    Winner: {{game.winners}}
-
-                </div>    
-                
-                <div class="my-20">
-                    Numbers Called: {{game.winningNumbers}}
-                    <!-- {{game.winningNumbers.sort((a,b) => a-b > 1 ? 1 :-1)}} -->
-                </div>
-                
-                <div class="my-10">
-                    <button @click="callNumber(game.id, game.winningNumbers)"
-                                class="p-2 m-2 text-white text-xl bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl "
-                        >
-                            Call New Number
-                    </button>
-                    <button @click="endCurrentGame(game.id)"
-                            class="p-2 m-2 text-white text-xl bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl "
-                    >
-                        End Game
-                    </button>
-                </div>
-                
-
-                <div class="container">
-                    Players called Bingo:
-                    <div v-for="bingo in playerBingos"
-                        :key="bingo.id"
-                        >
-                        <div>
-                            {{bingo.relayHandle}} - {{bingo.edition}}
-                            <button @click="getMetaData(bingo.edition)"
-                                class="p-1 m-1 text-white text-sm bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl">
-                                Verify
-                            </button>
-                            <button @click="deleteUserBingo(bingo.id)"
-                                class="p-1 m-1 text-white text-sm bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl">
-                                Clear
-                            </button>
-                            <button @click="setWinner(game.id, bingo.relayHandle, bingo.ownerAddress, bingo.cardLocation)"
-                                class="p-1 m-1 text-white text-sm bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl">
-                                Confirm Bingo
-                            </button>
+                <div class="w-full">
+                      <div class="bg-white">
+                        <div class="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                        <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                            <span class="block">Last Number Pulled:</span>
+                            <span v-if="currentGame && currentGame[0].winningNumbers && currentGame[0].winningNumbers.length > 1" class="block">{{currentGame[0].winningNumbers[currentGame[0].winningNumbers.length -1]}}</span>
+                        </h2>
+                        <div class="mt-8 max-w-96 justify-center text-indigo-800 font-bold text-xl">
+                            <div v-if="currentGame && currentGame[0].winningNumbers && currentGame[0].winningNumbers.length > 1" class="max-w-96 break-all">
+                                <!-- <span v-for="number in currentGame[0].winningNumbers.sort((a,b)=> { parseInt(a, 10) - parseInt(b, 10) ?  1 : -1})" :key="number" class='px-1 '>{{number}} </span> -->
+                                {{winningNumbers}}
+                            </div>
                         </div>
-                    </div>
+                        <div class="mt-8 flex justify-center">
+                            <div class="inline-flex rounded-md shadow">
+                                <button @click="callNumber(game.id, game.winningNumbers)"
+                                            class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                                    >
+                                        Roll Number
+                                </button>
+                            
+                            </div>
+                            <div class="ml-3 inline-flex">
+                                <button @click="endCurrentGame(game.id)"
+                                    class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+                                >
+                                    End Game
+                                </button>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="text-gray-900 text-lg container break-normal">
+                            Winner: {{game.winners.toString()}}
+                        </div> 
+                         <div class="container bg-gray-600 w-full p-4 rounded">
+                            Players called Bingo:
+                            <div v-for="bingo in playerBingos"
+                                :key="bingo.id"
+                                >
+                                <div>
+                                    {{bingo.relayHandle}} - {{bingo.edition}}
+                                    <button @click="getMetaData(bingo.edition)"
+                                        class="p-1 m-1 text-white text-sm bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl">
+                                        Verify
+                                    </button>
+                                    <button @click="deleteUserBingo(bingo.id)"
+                                        class="p-1 m-1 text-white text-sm bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl">
+                                        Clear
+                                    </button>
+                                    <button @click="setWinner(game.id, bingo.relayHandle, bingo.ownerAddress, bingo.cardLocation)"
+                                        class="p-1 m-1 text-white text-sm bg-gradient-to-r from-blue-500 to-indigo-500 rounded-xl">
+                                        Confirm Bingo
+                                    </button>
+                                </div>
+                            </div>
+                        </div>  
+                    </div> 
+                   
                 </div>
+                
 
 
                 <div class="container">
@@ -108,8 +115,9 @@ Create New Game
 import { ref } from 'vue'
 import { useBingo } from '../services/firebase'
 import { useStore } from 'vuex'
-
+import Menu from "./../components/Menu.vue"
 export default {
+    components:{ Menu },
     setup() {
 
         const store = useStore()
@@ -144,7 +152,7 @@ export default {
             this.$store.commit("setBingoCurrenGame", 0)
         },
         callNumber(gameId, nums) {
-            const newNum = Math.floor(Math.random() * 90) + 1
+            const newNum = this.getRandom(nums)
             if ( nums.includes(newNum) ) {
                 alert('duplicate number')
                 return
@@ -152,15 +160,21 @@ export default {
             this.setWinningNumber(gameId, newNum)
             this.$store.commit("setBingoCurrenGame", gameId)
         },
+        getRandom(nums){
+            let dup = false; 
+            let newNum = Math.floor(Math.random() * 90) + 1
+            if(nums.includes(newNum)){dup = true}
+            while(dup){
+                dup = false;
+                newNum = Math.floor(Math.random() * 90) + 1
+                if(nums.includes(newNum)){dup = true}
+            }
+            return newNum;
+        },
         async getMetaData(edition) {
             
             const metaData = await  import(`@/assets/metadata/${edition}`)
-            
-            // .then((module) => {
-            //     edition.value = module.default
-            // })
             this.meta = metaData
-
             console.log('here', metaData)
 
             
@@ -178,10 +192,16 @@ export default {
                 return ""
             }
             
+        },
+    }, 
+    computed:{
+            winningNumbers(){
+                if(this.currentGame[0].winningNumbers.length > 0){
+                    let str = this.currentGame[0].winningNumbers.toString()
+                    return str.replace(/,/g, ' ')
+                }
+                return [];
+            }
         }
-        
-    }
-    
-    
 }
 </script>
