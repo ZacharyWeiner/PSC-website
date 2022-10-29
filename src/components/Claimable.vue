dba2f6e1ebac25a3d97bdc235e859c1dcb1b712d
Zack Wins <el_hefe@mbp.attlocal.net>
2022-10-29 09:19:57 -0400

    updated the lastest claim date

--- src/components/Claimable.vue
+++ src/components/Claimable.vue
@@ -204,7 +204,7 @@ export default {
             if(this.isLogin  && this.$store.state.user_jigs.length > 0){
                 _canClaim = true;
             } 
-            let compareDate = new Date('2022-10-17');
+            let compareDate = new Date('2022-10-28');
             this.userActions['userActions'].value.forEach((ua)=> {
                 let actionDate = new Date(ua.timestamp.seconds * 1000);
                 if( actionDate > compareDate){
